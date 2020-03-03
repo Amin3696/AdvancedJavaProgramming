4d5709acc670047137780d90a22072184eab7889
shahimmo <mohammadamin.shahimian@drei.com>
2020-03-03 16:44:28 +0100

    advancedJavaProgramming lesson2 varargs

---
+++ src/varargs/Varargs.java
@@ -0,0 +1,35 @@
+package varargs;
+
+/**
+ * @author Amin Shahimian
+ */
+public class Varargs {
+    public static void main(String[] args) {
+        String item1 = "apple";
+        String item2 = "bread";
+        String item3 = "milk";
+        String item4 = "orange";
+        String[] shoppingList = {"koskos", "homus", "kornspitz", "yoguhrt"};
+        printShoppingList(item1, item2, item3, item4);
+        printShoppingList(item1, item2);
+        printShoppingList(shoppingList);
+
+    }
+
+    //St may need to write a method which should be executable with different number of arguments
+    //we use Variable length arguments (varargs) and write one method instead of
+    //overwrite the several methods with different number of args
+
+    private static void printShoppingList(String... item) {
+        System.out.println("Shopping list:");
+        /*for (String item : item) {
+            System.out.println(item);
+*/
+        for (int i = 0; i < item.length; i++) {
+            System.out.println(i + 1 + ":" + item[i]);
+        }
+    }
+}
+
+   
+
