baad13430a807ef8cfa4c10c61bbb3100df3b1d6
shahimmo <mohammadamin.shahimian@drei.com>
2020-03-04 10:54:18 +0100

    lesson2 update House in substitution_principle package

--- src/chapter02/substitution_principle/House.java
+++ src/chapter02/substitution_principle/House.java
@@ -11,4 +11,12 @@ public class House extends Building {
     public String toString() {
         return ("house");
     }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
 }
